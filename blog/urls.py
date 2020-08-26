b1d496894d4a4d5cac563e48cc4a89c0cb2d319a
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-08-25 23:06:09 -0300

    Remove unecessary views and urls paths.

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,8 @@ app_name="blog"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("search/", views.search, name="search"),
+    path("article/search/", views.search, name="search"),
     path("register/", views.sign_in, name="register"),
     path("login/", views.login, name="login"),
-    path("which_user/", views.which_user, name="which_user"),
-    path("logout/", views.logout, name="logout"),
-    path("<str:str>/", views.show_post, name="show_post")
+    path("article/<str:str>/", views.show_post, name="show_post")
 ]
